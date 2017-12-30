0b28cac26eece556c078e693dcbe9ece65c49c60
Igor Perkovsky <igor.perkovsky@mail.ru>
2017-12-30 10:13:09 +0200

    Added button "Add random user"

--- WebAppDBTestDocker/Controllers/HomeController.cs
+++ WebAppDBTestDocker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using WebAppDBTestDocker.Models;
 
 namespace WebAppDBTestDocker.Controllers
@@ -10,5 +11,29 @@ namespace WebAppDBTestDocker.Controllers
         public HomeController(IUserRepository repository) => this.repository = repository;
 
         public IActionResult Index() => View(repository.Users);
+
+        private int GetRandomNumber(int max = 100)
+        {
+            var rand = new Random();
+            return rand.Next((max == 100) ? 10 : 100, max);
+        }
+
+        private User GetNewUser()
+        {
+            User user = new Models.User
+            {
+                Name = Guid.NewGuid().ToString().Substring(0, 8),
+                Age = GetRandomNumber(),
+                Phone = $"({GetRandomNumber(1000)}) {GetRandomNumber(1000)}-{GetRandomNumber()}-{GetRandomNumber()}"
+            };
+            return user;
+        }
+
+        [HttpPost]
+        public RedirectToActionResult AddRandomUser()
+        {
+            repository.SaveUser(GetNewUser());
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
